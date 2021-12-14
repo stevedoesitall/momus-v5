8b253d635a6ac39361f607a6af57b171feaa677b
Steve Giordano <stephenagiordano@gmail.com>
2021-12-14 12:53:37 -0500

    fixing

---
+++ types/users.model.ts
@@ -0,0 +1,22 @@
+interface CreateUser {
+    id: string;
+    user_name: string;
+    password: string;
+    email?: string;
+    last_pageview?: string;
+    last_update_date?: Date;
+    latest_session_id?: string;
+    is_admin?: boolean;
+    is_verified?: boolean;
+    token_time?: Date;
+    verify_time?: Date;
+    token?: string;
+    reset_token?: string;
+    users_tweets: string[];
+}
+
+interface UpdateUser extends Partial<CreateUser> {
+    id: string;
+}
+
+export { CreateUser, UpdateUser };
